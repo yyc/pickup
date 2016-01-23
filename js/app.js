31c674b4b872e479a1d38cab44512d3054eadb29
nam <namjjd@gmail.com>
2016-01-23 19:13:01 +0800

    Testing test

--- js/app.js
+++ js/app.js
@@ -480,17 +480,36 @@ var SonicSocket = require('./lib/sonic-socket.js');
 var SonicServer = require('./lib/sonic-server.js');
 var SonicCoder = require('./lib/sonic-coder.js');
 function PickUp() {
-    alert("hello it's me");
-    var sonicSoc = 1;
-    var sonicSer = 2;
+  this.sonicSocket = undefined;
+  this.sonicServet = undefined;
+  var sonicSoc = 1;
+  var sonicSer = 2;
+  createSonicNetwork();
 }
-PickUp.prototype.xx = 3;
+
+function createSonicNetwork(opt_coder) {
+  // Stop the sonic server if it is listening.
+  var ALPHABET = "temp";
+  this.sonicServer = new SonicServer({alphabet: ALPHABET, debug: false});
+  this.sonicSocket = new SonicSocket({alphabet: ALPHABET});
+
+  this.sonicServer.start();
+  console.log(this.sonicServer);
+  this.sonicServer.on('message', hihi);
+}
+function hihi(x) {
+  alert("MESSAGE RECEIVED");
+  console.log(x);
+}
 //Listening
 PickUp.prototype.listenFor = function(tone, duration, callback) {
-
     callback();
 }
 
+function hello(){
+
+
+}
 PickUp.prototype.listenForSequence = function(tones, duration, callback) {
 
 
