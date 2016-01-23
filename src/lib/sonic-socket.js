18c84d95a17c1096c7fdbdb73889bf0b6c28e766
nam <namjjd@gmail.com>
2016-01-23 22:42:20 +0800

    asdf

--- src/lib/sonic-socket.js
+++ src/lib/sonic-socket.js
@@ -21,7 +21,7 @@ function SonicSocket(params) {
 SonicSocket.prototype.send = function(input, opt_callback) {
   // Surround the word with start and end characters.
   //input = this.coder.startChar + input + this.coder.endChar;
-  console.log("inputlen: "+input.length);
+  /*console.log("inputlen: "+input.length);
   paraminput = input;
   input = this.coder.startChar;
   for(var i = 0; i < paraminput.length - 1; i++) {
@@ -30,7 +30,7 @@ SonicSocket.prototype.send = function(input, opt_callback) {
   input += paraminput[paraminput.length - 1] + this.coder.endChar;
 
   console.log("inputlen: "+input.length);
-  
+  */
   // Use WAAPI to schedule the frequencies.
   for (var i = 0; i < input.length; i++) {
     var char = input[i];
