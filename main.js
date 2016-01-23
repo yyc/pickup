30f5de081a59ae9829e5a67f35e21d30672fba4b
nam <namjjd@gmail.com>
2016-01-23 15:53:24 +0800

    methods

--- main.js
+++ main.js
@@ -1,6 +1,43 @@
 var SonicNet = require("./lib/build/sonicnet.js")
 
+function PickUp() {
 
-module.exports = {
-    SN: SonicNet;    
+
+
+
 }
+//Listening
+PickUp.prototype.listenFor(tone, duration, callback) {
+
+    callback();
+}
+
+PickUp.prototype.listenForSequence(tones, duration, callback) {
+
+
+    callback();
+}
+
+//Broadcasting
+
+PickUp.prototype.broadcast(message, callback) {
+
+    callback();
+}
+
+PickUp.prototype.broadcast(message, options, callback) {
+
+    callback();
+}
+
+//Both
+
+PickUp.prototype.broadcastAcknowledge(message, permittedResponses, callback) {
+
+
+}
+
+
+//Others
+
+module.exports = PickUp;
